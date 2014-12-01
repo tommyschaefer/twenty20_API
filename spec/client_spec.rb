35eb8cf2c1e796b7d2df46f0c947454f91478248
Alex Wheeler <afwheeler92@gmail.com>
2014-12-01 13:57:21 -0500

    adds vcr gem and webmock to record and stub out external api requests

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Twenty20::Client do
-
   describe "BASE_URI" do
     it "returns the base uri for the api" do
       expect(Twenty20::Client::BASE_URI).to eq("https://api-v2.twenty20.com/")
@@ -9,34 +8,29 @@ describe Twenty20::Client do
   end
 
   describe "get_featured_items" do
-
     before(:all) do
       @client = Twenty20::Client.new
     end
 
     it "returns an Array" do
-      expect(@client.get_featured_items.class).to eq(Array)
+      VCR.use_cassette('get_featured_items') do
+        expect(@client.get_featured_items.class).to eq(Array)
+      end
     end
 
     it "returns an Array of Item objects" do
-      expect(@client.get_featured_items[0].class).to eq(Twenty20::Item)
-    end
-
-    it "returns error if status is not 200" do
-      fake_response = double
-      fake_response.stub(:code) { 400 }
-      Twenty20::Client.stub(:get)  { fake_response }
-      response = @client.get_featured_items
-      expect(response).to eq("Error")
+      VCR.use_cassette('get_featured_items') do
+        expect(@client.get_featured_items[0].class).to eq(Twenty20::Item)
+      end
     end
 
     it "can accept a block and do something cool like push challenges into a new array" do
       val = []
-      @client.get_featured_items {|item| val.push(item)}
-      expect(val.empty?).to_not eq(true)
-
+      VCR.use_cassette("get_featured_items") do
+        @client.get_featured_items {|item| val.push(item)}
+        expect(val.empty?).to_not eq(true)
+      end
     end
-
   end
 
   describe "get_challenges" do
@@ -45,14 +39,18 @@ describe Twenty20::Client do
     end
 
     it "should return an array of challenge objects" do
-      response = @client.get_challenges
-      expect(response.class).to eq(Array);
+      VCR.use_cassette("get_challenges") do
+        response = @client.get_challenges
+        expect(response.class).to eq(Array);
+      end
     end
 
     it "can accept a block and do something cool like push challenges into a new array" do
-      val = []
-      @client.get_challenges {|challenge|  val.push(challenge)}
+      VCR.use_cassette("get_challenges") do 
+        val = []
+        @client.get_challenges {|challenge|  val.push(challenge)}
       expect(val.empty?).to_not eq(true)
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,15 @@
 require 'twenty20'
+require 'webmock/rspec'
+require 'vcr'
+WebMock.disable_net_connect!(allow_localhost: true)
+
+VCR.configure do |c|
+    c.cassette_library_dir = 'fixtures/vcr_cassettes'
+    c.hook_into :webmock # or :fakeweb
+end
+
+
+RSpec.configure do |config|
+  config.expose_current_running_example_as :example
+  config.raise_errors_for_deprecations!
+end
