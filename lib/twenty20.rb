f8776073c2ae8713028965c1a8f5d90219de5c61
Tommy Schaefer <tommy.schaefer@teecom.com>
2014-12-02 22:41:47 -0600

    Clean up client class and include active support

--- lib/twenty20.rb
+++ lib/twenty20.rb
@@ -1,63 +1,53 @@
-require "twenty20/version"
-require "twenty20/api_object"
-require "twenty20/item"
-require "twenty20/challenge"
-require "httparty"
+require 'twenty20/version'
+require 'twenty20/api_object'
+require 'twenty20/item'
+require 'twenty20/challenge'
+require 'httparty'
+require 'active_support/inflector'
+require 'active_support/core_ext/hash/conversions'
 
 module Twenty20
-  class Client 
+  class Client
     include HTTParty
-    BASE_URI = "https://api-v2.twenty20.com/"
-    
+
+    BASE_URI = 'https://api-v2.twenty20.com/'
+
     def get_featured_items(&block)
-      featured_route = "items/featured"
-      featured_params = "?featured=true"
-      resource = build_route(featured_route, featured_params) 
-      response = self.class.get(resource)
-      if(response.code == 200)
-        item_collection = to_collection(response, "item")
-        if(block_given?)
-          item_collection.each do |item|
-            block.call(item)
-          end
-        else
-          item_collection
-        end
-      else
-        "Error"
-      end
+      resource = build_route('items/featured', featured: true)
+      response_for(resource, :item, &block)
     end
 
     def get_challenges(&block)
-      challenges_route = "/challenges/open-for-submissions"
-      resource = build_route(challenges_route)
-      response = self.class.get(resource) 
-      if(response.code == 200)
-        challenge_collection = to_collection(response, "challenge")
-        if block_given?
-          challenge_collection.each do |challenge|
-            block.call(challenge)
-          end 
-        else
-          challenge_collection
-        end
-      else
-        "Error"
-      end
+      resource = build_route('/challenges/open-for-submissions')
+      response_for(resource, :challenge, &block)
     end
 
     private
 
-    def to_collection(response, collection_class)
-      response.parsed_response[collection_class + "s"].collect do |e|
-        o = Object.const_get("Twenty20::" + collection_class.capitalize) #creates new array full of collection_class object (i.e. Items, Challenges)
-        o.new(e)
+    def response_for(resource, type, &block)
+      response = self.class.get(resource)
+
+      return 'Error' unless response.code == 200
+
+      collection = to_collection(response, type)
+
+      return collection unless block_given?
+
+      collection.each do |item|
+        block.call(item)
       end
     end
 
-    def build_route(uri, params = "")
-      route = BASE_URI + uri + params
-      route
+    def to_collection(response, collection_class)
+      collection_class = collection_class.to_s
+
+      response.parsed_response[collection_class.pluralize].map do |e|
+        "Twenty20::#{collection_class.singularize.classify}".constantize.new(e)
+      end
+    end
+
+    def build_route(uri, params = {})
+      BASE_URI + uri + '?' + params.to_query
     end
   end
 end
--- twenty20.gemspec
+++ twenty20.gemspec
@@ -26,4 +26,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec", "~> 2.8"
   spec.add_development_dependency "webmock", "~>1.0"
   spec.add_development_dependency "vcr", "~>2.9"
+  spec.add_development_dependency "activesupport"
 end
