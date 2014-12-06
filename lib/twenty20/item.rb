ca4d84482b16c36c827b0ff21072982e471619bd
Tommy Schaefer <tommy.schaefer@teecom.com>
2014-12-05 22:36:09 -0600

    Fix typo on item class

--- lib/twenty20/item.rb
+++ lib/twenty20/item.rb
@@ -1,14 +1,15 @@
 module Twenty20
   class Item < ApiObject
     def self.attributes
-      %w(caption id
+      %w(caption
+         id
          source_name
          image_url
          featured_at
          user_first_name
          user_last_name
          user_avatar_url
-         usename
+         username
          display_name)
     end
   end
